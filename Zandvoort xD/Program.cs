163826992cdd385939a0be2c11d67582c478d30a
TauTauwk <s1168742@student.windesheim.nl>
2022-09-21 15:21:41 +0200

    bijna

--- Zandvoort xD/Program.cs
+++ Zandvoort xD/Program.cs
@@ -11,18 +11,21 @@ namespace Zandvoort_xD // Note: actual namespace depends on the project name.
         {
             Initialize();
             NextRace();
-            Console.WriteLine("Track: " + CurrentRace.Track.Name + "\n");
-            CurrentRace.RandomizeEquipment();
-            Console.WriteLine("Name \t\tColor \tPerformance \tQuality \tSpeed \tBroken");
-            foreach (var Participant in CurrentRace.Participants)
-            {
-                Console.WriteLine(Participant.Name + "\t\t" 
-                                + Participant.TeamColor + "\t" 
-                                + Participant.Equipment.Performance + "\t\t" 
-                                + Participant.Equipment.Quality + "\t\t" 
-                                + Participant.Equipment.Speed + "\t" 
-                                + Participant.Equipment.IsBroken);
-            }
+            #region trackNameTest
+            //Console.WriteLine("Track: " + CurrentRace.Track.Name + "\n");
+            //CurrentRace.RandomizeEquipment();
+            //Console.WriteLine("Name \t\tColor \tPerformance \tQuality \tSpeed \tBroken");
+            //foreach (var Participant in CurrentRace.Participants)
+            //{
+            //    Console.WriteLine(Participant.Name + "\t\t" 
+            //                    + Participant.TeamColor + "\t" 
+            //                    + Participant.Equipment.Performance + "\t\t" 
+            //                    + Participant.Equipment.Quality + "\t\t" 
+            //                    + Participant.Equipment.Speed + "\t" 
+            //                    + Participant.Equipment.IsBroken);
+            //}
+            #endregion
+            Virtualization.DrawTrack(CurrentRace.Track);
 
             for (; ; )
             {
--- Zandvoort xD/virtualization.cs
+++ Zandvoort xD/virtualization.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -35,6 +36,15 @@ namespace Zandvoort_xD
 
         private static string[] _LeftHorizontal =
         {
+            "/   |",
+            " # #|",
+            "    /",
+            "   / ",
+            "--/  "
+        };
+
+        private static string[] _RightHorizontal =
+        {
             "--\\  ",
             "   \\ ",
             "    \\",
@@ -42,8 +52,98 @@ namespace Zandvoort_xD
             "\\   |"
         };
 
-        //private static string[] _RightHorizontal =
+        private static string[] _StartGrid =
+        {
+            "-----",
+            "  >#>",
+            "     ",
+            " > # ",
+            "-----"
+        };
 
         #endregion
+
+        private static string[] mirrorHorizontal(string[] _section)
+        {
+            string[] result = new string[4];
+            result[0] = _section[4];
+            result[1] = _section[3];
+            result[2] = _section[2];
+            result[3] = _section[1];
+            result[4] = _section[0];
+            return result;
+        }
+
+        private static string[] mirrorVertical(string[] _section)
+        {
+            int num = 0;
+            char[] [] ch = new char[4][];
+            foreach (string s in _section)
+            {
+                char[] sectionDetails = s.ToCharArray();
+                ch[num] = sectionDetails;
+
+                string charStr = new string(sectionDetails);
+                num++;
+            }
+        }
+        public static void DrawTrack(Track track)
+        {
+            int x = 25;
+            int y = 24;
+
+            Console.SetWindowSize(49, 49);
+            Console.SetCursorPosition(x,y);
+
+            foreach (var section in track.Sections) 
+            {
+                if (section.SectionType == SectionTypes.LeftCorner)
+                {
+                    foreach (string s in _LeftHorizontal)
+                    {
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(s);
+                    }
+                }
+                if (section.SectionType == SectionTypes.RightCorner)
+                {
+                    foreach (string s in _RightHorizontal)
+                    {
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(s);
+                    }
+                }
+                if (section.SectionType == SectionTypes.StartGrid)
+                {
+                    foreach (string s in _StartGrid)
+                    {
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(s);
+                    }
+                }
+                if (section.SectionType == SectionTypes.Straight)
+                {
+                    foreach (string s in _StraightHorizontal)
+                    {
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(s);
+                    }
+                }
+                if (section.SectionType == SectionTypes.Finish)
+                {
+                    foreach (string s in _finishHorizontal)
+                    {
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine(s);
+                    }
+                }
+
+            }
+        }
     }
 }
