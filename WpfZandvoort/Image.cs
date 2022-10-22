194ec3493bddd89d10a7b1ebfe3cfb682b96c1e9
TauTauwk <s1168742@student.windesheim.nl>
2022-10-22 15:08:06 +0200

    6.5 done
    wanted to work on a different pc hope everything gets copied

---
+++ WpfZandvoort/Image.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfZandvoort
+{
+    public static class Image
+    {
+        private static Dictionary<string, Bitmap> _images = new();
+
+        public static Bitmap GetImage(string ImageLocation)
+        {
+            if (!_images.ContainsKey(ImageLocation))
+            {
+                _images.Add(ImageLocation, new Bitmap(ImageLocation));
+            }
+            return (Bitmap)_images[ImageLocation].Clone();
+        }
+
+        public static void ClearImageCache()
+        {
+            _images.Clear();
+        }
+
+        public static Bitmap DrawBitmap(int x, int y)
+        {
+            string key = "empty";
+            if (!_images.ContainsKey(key))
+            {
+                _images.Add(key, new Bitmap(x,y));
+                Graphics graphics = Graphics.FromImage(_images[key]);
+                graphics.Clear(System.Drawing.Color.DarkGray);
+            }
+            return (Bitmap)_images[key].Clone();
+            
+        }
+
+        //converteerd Bitmap naar BitmapSource dit moet omdat je geen Bitmap kan gebruiken in een WPFwindow
+        public static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+
+    }
+}
---
+++ WpfZandvoort/Visualization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfZandvoort
+{
+    public static class Visualization
+    {
+        public Bitmap Graphics(int x, int y)
+        {
+            return null;
+        }
+    }
+}
